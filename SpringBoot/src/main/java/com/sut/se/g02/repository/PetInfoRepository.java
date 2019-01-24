5ca7270c688b443bed34438c72fe1794ca5be804
ponlm225 <ponlm225@gmail.com>
2019-01-25 00:24:08 +0700

    ทำการเพิ่ม Repository ของระบบย่อยบันทึกประวัติ -close #52

---
+++ SpringBoot/src/main/java/com/sut/se/g02/repository/PetInfoRepository.java
@@ -0,0 +1,11 @@
+package com.sut.se.g02.repository;
+
+import com.sut.se.g02.entity.PetInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public interface PetInfoRepository extends JpaRepository<PetInfo, Long> {
+}
