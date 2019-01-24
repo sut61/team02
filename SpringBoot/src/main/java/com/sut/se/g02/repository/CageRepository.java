21371481f4741478e8191fc85a5cf29f796c02e4
pdxcnk <JH_Joker89@hotmail.com>
2019-01-25 01:27:32 +0700

    ทำการเพิ่ม Repository ของระบบย่อย ระบบพักฟ้นสัตว์ -close #54

---
+++ SpringBoot/src/main/java/com/sut/se/g02/repository/CageRepository.java
@@ -0,0 +1,14 @@
+package com.sut.se.g02.repository;
+
+import com.sut.se.g02.entity.Cage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+public
+interface CageRepository extends JpaRepository<Cage,Long> {
+}
