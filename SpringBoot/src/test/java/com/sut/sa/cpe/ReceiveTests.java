0c80fc5342d5e1acfa05edd9b6d5ceeca05280c7
thawatchainaja <Thawatchai_nongruang@hotmail.com>
2019-02-09 20:35:18 +0700

    ทำการเพิ่ม Test Case ของระบบย่อยรับยา -close #112

---
+++ SpringBoot/src/test/java/com/sut/sa/cpe/ReceiveTests.java
@@ -0,0 +1,142 @@
+package com.sut.se.g02;
+
+import lombok.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Collections;
+import java.util.OptionalInt;
+import java.util.Set;
+import java.util.Date;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.sql.Timestamp;
+import javax.persistence.*;
+
+import com.sut.se.g02.repository.*;
+import com.sut.se.g02.entity.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+
+public class ReceiveTests {
+
+
+    @Autowired
+    private ReceiveMedicineRepository receiveMedicineRepository;
+
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private Validator validator;
+
+    @Before
+    public void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+    @Test
+    public void FinishReceive() {
+        ReceiveMedicine receiveMedicine = new ReceiveMedicine();
+        receiveMedicine.setNoteNew("โอ้พระเจ้า00-7");
+
+
+        try {
+            entityManager.persist(receiveMedicine);
+            entityManager.flush();
+            //fail("Should not pass to this line");
+            System.out.println();
+            System.out.println("----------> Finish  Compile Receive <--------------------");
+            //System.out.println(e.getMessage());
+            System.out.println();
+            System.out.println();
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+
+        }
+
+    }
+
+    @Test
+    public void CannotNullReceive() {
+        ReceiveMedicine receiveMedicine = new ReceiveMedicine();
+        receiveMedicine.setNoteNew(null);
+
+
+        try {
+            entityManager.persist(receiveMedicine);
+            entityManager.flush();
+
+            fail("Should not pass to this line");
+        } catch(javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+            System.out.println();
+            System.out.println("----------> Notnull NoteNew<--------------------");
+            System.out.println(e.getMessage());
+            System.out.println();
+            System.out.println();
+        }
+
+    }
+
+    @Test
+    public void testPatternReceive() {
+        ReceiveMedicine receiveMedicine = new ReceiveMedicine();
+        receiveMedicine.setNoteNew("ggyes");
+
+        try {
+            entityManager.persist(receiveMedicine);
+            entityManager.flush();
+            fail("Should not pass to this line");
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+            System.out.println();
+            System.out.println("----------> Comment Pattern Receive <--------------------");
+            System.out.println(e.getMessage());
+            System.out.println();
+            System.out.println();
+        }
+    }
+    @Test
+    public void testSizeReceive() {
+        ReceiveMedicine receiveMedicine = new ReceiveMedicine();
+        receiveMedicine.setNoteNew("หหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหหห");
+
+        try {
+            entityManager.persist(receiveMedicine);
+            entityManager.flush();
+            fail("Should not pass to this line");
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+            System.out.println();
+            System.out.println("----------> Comment Sizelong Receive<--------------------");
+            System.out.println(e.getMessage());
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
