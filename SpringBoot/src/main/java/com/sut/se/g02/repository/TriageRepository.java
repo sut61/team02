c1bb0a321df96d6eee39aea7c3481ee2449e47e3
fizimal <sasitawongphayak@gmail.com>
2019-02-21 22:12:43 +0700

    ทำการแก้ไข Repository ของระบบคัดแยกอาการ -close #179

--- SpringBoot/src/main/java/com/sut/se/g02/repository/TriageRepository.java
+++ SpringBoot/src/main/java/com/sut/se/g02/repository/TriageRepository.java
@@ -8,4 +8,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface TriageRepository extends JpaRepository<Triage, Long> {
+	Triage findByTriageId(Long triageId);
 }
