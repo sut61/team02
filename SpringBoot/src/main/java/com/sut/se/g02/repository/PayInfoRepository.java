dc5c2f9af4107efe910316bf2239006c441a8503
nknawarat99 <keaw2939@gmail.com>
2019-02-21 22:46:41 +0700

    ทำการแก้ไข Repository ของระบบแจ้งชำระค่ารักษาพยาบาล -close #159

--- SpringBoot/src/main/java/com/sut/se/g02/repository/PayInfoRepository.java
+++ SpringBoot/src/main/java/com/sut/se/g02/repository/PayInfoRepository.java
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface PayInfoRepository extends JpaRepository<PayInfo, Long> {
-
+			PayInfo findByPayInfoId(Long payInfoId);
 }
