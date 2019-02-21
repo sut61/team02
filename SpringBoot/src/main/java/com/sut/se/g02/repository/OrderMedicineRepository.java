2a9afb40a2fb2dbac01f18147d2da25f01b5f1e1
thawatchainaja <Thawatchai_nongruang@hotmail.com>
2019-02-22 01:09:57 +0700

    ทำการแก้ไข Repository ของระบบสั่งซื้อยา -close #207

--- SpringBoot/src/main/java/com/sut/se/g02/repository/OrderMedicineRepository.java
+++ SpringBoot/src/main/java/com/sut/se/g02/repository/OrderMedicineRepository.java
@@ -10,4 +10,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface OrderMedicineRepository extends JpaRepository<OrderMedicine,Long> {
+	OrderMedicine findByOrderMedicineId(Long orderMedicineId);
 }
