f4b412645473bc549f6d8bcbd32904140b731711
thawatchainaja <Thawatchai_nongruang@hotmail.com>
2019-02-22 01:36:39 +0700

    ทำการแก้ไข Repository ของระบบรับยา -close #212

--- SpringBoot/src/main/java/com/sut/se/g02/repository/CheckStatusRepository.java
+++ SpringBoot/src/main/java/com/sut/se/g02/repository/CheckStatusRepository.java
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface CheckStatusRepository extends JpaRepository<CheckStatus,Long> {
-
+		CheckStatus findByCheckStatusId(Long checkStatusId);
 }
