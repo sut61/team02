c7e66d58db18758c17348dbcd4059afd13a9350d
ponlm225 <ponlm225@gmail.com>
2019-02-21 21:35:27 +0700

    ทำการแก้ไข Repository ระบบบริการ -close #166

--- SpringBoot/src/main/java/com/sut/se/g02/repository/ServeRepository.java
+++ SpringBoot/src/main/java/com/sut/se/g02/repository/ServeRepository.java
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ServeRepository extends JpaRepository<Serve,Long>{
-
+		Serve findByServeId(Long serveId);
 }
